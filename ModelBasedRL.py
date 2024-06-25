c9b4286c72a6dc7fd4194978dc45e79804a1606a
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-06-26 00:33:23 +0300

    fix

--- ModelBasedRL.py
+++ ModelBasedRL.py
@@ -1,55 +1,77 @@
 import numpy as np
+from GridWorldIterator import *
 
 class ModelBasedAgent:
-    def __init__(self, grid, rewards, p, r, discount_factor=0.5):
-        self.h, self.w = len(grid), len(grid[0])
+    def __init__(self, w, h, rewards, p, r, discount_factor=0.5):
+        self.h, self.w = h, w
         self.rewards = rewards
         self.p = p
         self.r = r
         self.discount_factor = discount_factor
         self.value_function = np.zeros((self.h, self.w))
-        self.policy = np.zeros((self.h, self.w, 4))  # 4 possible actions: up, down, left, right
+        self.policy = np.full((self.h, self.w), '', dtype=object)  # Initialize with empty strings
 
         # Convert rewards into a 2D array
         self.reward_grid = np.full((self.h, self.w), r)
         for x, y, reward in self.rewards:
-            self.reward_grid[y, x] = reward
+            self.reward_grid[self.h-y-1, x] = reward
+        self.max_reward = max(pos[2] for pos in rewards)
 
         self.actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # up, down, left, right
+        self.action_str = ['up', 'down', 'left', 'right']  # corresponding action strings
 
-    def learn(self, iterations=1000):
+    def learn(self, iterations=10000):
         for _ in range(iterations):
             new_value_function = np.copy(self.value_function)
             for y in range(self.h):
                 for x in range(self.w):
-                    if (x, y) in [(pos[0], pos[1]) for pos in self.rewards]:  # Skip terminal states
-                        continue
+                    if (x, y) in [pos[:2] for pos in self.rewards]:
+                        val = self.reward_grid[h-1-y, x]
+                        if self.max_reward == val:
+                            self.policy[h-y-1,x]='maxreward' 
+                        else: 
+                            self.policy[h-y-1,x]='reward' if val>0  else  'negreward' if val < 0  else 'unreward'
+                            continue
                     values = []
                     for action in self.actions:
-                        new_y, new_x = y + action[0], x + action[1]
+                        new_y, new_x = self.h-1-y + action[0], x + action[1]
                         if 0 <= new_y < self.h and 0 <= new_x < self.w:
                             values.append(self.p * (self.reward_grid[new_y, new_x] + self.discount_factor * self.value_function[new_y, new_x]) + 
-                                          (1 - self.p) / 2 * (self.reward_grid[y, x] + self.discount_factor * self.value_function[y, x]))
+                                          (1 - self.p) / 2 * (self.reward_grid[self.h-1-y, x] + self.discount_factor * self.value_function[self.h-1-y, x]))
                         else:
-                            values.append(self.reward_grid[y, x] + self.discount_factor * self.value_function[y, x])
-                    new_value_function[y, x] = max(values)
+                            values.append(self.reward_grid[self.h-1-y, x] + self.discount_factor * self.value_function[self.h-1-y, x])
+                    new_value_function[self.h-1-y, x] = max(values)
             self.value_function = new_value_function
 
         for y in range(self.h):
             for x in range(self.w):
-                if (x, y) in [(pos[0], pos[1]) for pos in self.rewards]:
-                    continue
+                if (x, y) in [pos[:2] for pos in self.rewards]:
+                    val = self.reward_grid[h-1-y, x]
+                    if self.max_reward == val:
+                        self.policy[h-y-1,x]='maxreward' 
+                    else: 
+                        self.policy[h-y-1,x]='reward' if val>0  else  'negreward' if val < 0  else 'unreward'
+                        continue
                 values = []
                 for action in self.actions:
-                    new_y, new_x = y + action[0], x + action[1]
+                    new_y, new_x = self.h-1-y + action[0], x + action[1]
                     if 0 <= new_y < self.h and 0 <= new_x < self.w:
                         values.append(self.p * (self.reward_grid[new_y, new_x] + self.discount_factor * self.value_function[new_y, new_x]) + 
-                                      (1 - self.p) / 2 * (self.reward_grid[y, x] + self.discount_factor * self.value_function[y, x]))
+                                      (1 - self.p) / 2 * (self.reward_grid[self.h-1-y, x] + self.discount_factor * self.value_function[self.h-1-y, x]))
                 best_action = np.argmax(values)
-                self.policy[y, x] = np.eye(4)[best_action]
+                self.policy[h-1-y, x] = self.action_str[best_action] if self.reward_grid[h-1-y, x]!=self.max_reward else 'maxreward' 
 
     def get_policy(self):
         return self.policy
 
     def get_value_function(self):
         return self.value_function
+
+if __name__ == "__main__":
+    iterator = GridWorldIterator('GridWorld.py')
+    for w, h, L, p, r in iterator:
+        md = ModelBasedAgent(w, h, L, p, r)
+        md.learn()
+        print("Policy:\n", md.get_policy())
+        print("Value Function:\n", md.get_value_function())
+        break
--- ValueIteration.py
+++ ValueIteration.py
@@ -1,122 +1,54 @@
-# import numpy as np
-
-# def value_iteration(w, h, rewards, p, r, discount_factor=0.5, theta=0.01):
-#     value_function = np.zeros((h, w))
-#     reward_grid = np.full((h, w), r)
-    
-#     for x, y, reward in rewards:
-#         reward_grid[y, x] = reward
-
-#     actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # up, down, left, right
-
-#     while True:
-#         delta = 0
-#         for y in range(h):
-#             for x in range(w):
-#                 if (x, y) in [(pos[0], pos[1]) for pos in rewards]:  # Skip terminal states
-#                     continue
-#                 v = value_function[y, x]
-#                 values = []
-#                 for action in actions:
-#                     new_y, new_x = y + action[0], x + action[1]
-#                     if 0 <= new_y < h and 0 <= new_x < w:
-#                         values.append(p * (reward_grid[new_y, new_x] + discount_factor * value_function[new_y, new_x]) + 
-#                                       (1 - p) / 2 * (reward_grid[y, x] + discount_factor * value_function[y, x]))
-#                     else:
-#                         values.append(reward_grid[y, x] + discount_factor * value_function[y, x])
-#                 value_function[y, x] = max(values)
-#                 delta = max(delta, abs(v - value_function[y, x]))
-
-#         if delta < theta:
-#             break
-
-#     policy = np.zeros((h, w, 4))  # 4 possible actions: up, down, left, right
-#     for y in range(h):
-#         for x in range(w):
-#             if (x, y) in [(pos[0], pos[1]) for pos in rewards]:
-#                 continue
-#             values = []
-#             for action in actions:
-#                 new_y, new_x = y + action[0], x + action[1]
-#                 if 0 <= new_y < h and 0 <= new_x < w:
-#                     values.append(p * (reward_grid[new_y, new_x] + discount_factor * value_function[new_y, new_x]) + 
-#                                   (1 - p) / 2 * (reward_grid[y, x] + discount_factor * value_function[y, x]))
-#                 else:
-#                     values.append(reward_grid[y, x] + discount_factor * value_function[y, x])
-#             best_action = np.argmax(values)
-#             policy[y, x] = np.eye(4)[best_action]
-
-#     return policy, value_function
-
-
-# if __name__ == "__main__":
-#     w = 4
-#     h = 3
-#     L = [(1, 1, 0), (3, 2, 1), (3, 1, -1)]
-#     p = 0.8
-#     r = -0.04
-#     policy, value_function = value_iteration(w, h, L, p, r)
-#     print("Policy:\n", policy)
-#     print("Value Function:\n", value_function)
-
-
 import numpy as np
+from GridWorldIterator import *
 
 def value_iteration(w, h, rewards, p, r, discount_factor=0.5, theta=0.01):
+    
     value_function = np.zeros((h, w))
     reward_grid = np.full((h, w), r)
     
     for x, y, reward in rewards:
         reward_grid[h-1-y, x] = reward
-
+        
     actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # up, down, left, right
     action_names = ["up", "down", "left", "right"]
+    policy = np.full((h, w), "", dtype=object)
+    max_reward = max(pos[2] for pos in rewards)
 
     while True:
         delta = 0
         for y in range(h):
             for x in range(w):
-                if (x, h-1-y) in [(pos[0], pos[1]) for pos in rewards]:  # Skip terminal states
-                    continue
-                v = value_function[y, x]
+                if (x, y) in [pos[:2] for pos in rewards]:
+                    val = reward_grid[h-1-y, x]
+                    if max_reward == val:
+                        policy[h-y-1,x]='maxreward' 
+                    else: 
+                        policy[h-y-1,x]='reward' if val>0  else  'negreward' if val < 0  else 'unreward'
+                        continue
+                    
+                v = value_function[h-1-y, x]
                 values = []
                 for action in actions:
-                    new_y, new_x = y + action[0], x + action[1]
+                    new_y, new_x = h-1-y + action[0], x + action[1]
                     if 0 <= new_y < h and 0 <= new_x < w:
-                        values.append(p * (reward_grid[h-1-new_y, new_x] + discount_factor * value_function[new_y, new_x]) + 
-                                      (1 - p) / 2 * (reward_grid[h-1-y, x] + discount_factor * value_function[y, x]))
+                        values.append(p * (reward_grid[new_y, new_x] + discount_factor * value_function[new_y, new_x]) + 
+                                      (1 - p) / 2 * (reward_grid[h-1-y, x] + discount_factor * value_function[h-1-y, x]))
                     else:
-                        values.append(reward_grid[h-1-y, x] + discount_factor * value_function[y, x])
-                value_function[y, x] = max(values)
-                delta = max(delta, abs(v - value_function[y, x]))
-
+                        values.append(reward_grid[h-1-y, x] + discount_factor * value_function[h-1-y, x])
+                value_function[h-1-y, x] = max(values)
+                delta = max(delta, abs(v - value_function[h-1-y, x]))
+                best_action = np.argmax(values)
+                policy[h-1-y, x] = action_names[best_action] if reward_grid[h-1-y, x]!=max_reward else 'maxreward' 
+                
         if delta < theta:
             break
-
-    policy = np.full((h, w), "", dtype=object)
-    for y in range(h):
-        for x in range(w):
-            if (x, h-1-y) in [(pos[0], pos[1]) for pos in rewards]:
-                continue
-            values = []
-            for action in actions:
-                new_y, new_x = y + action[0], x + action[1]
-                if 0 <= new_y < h and 0 <= new_x < w:
-                    values.append(p * (reward_grid[h-1-new_y, new_x] + discount_factor * value_function[new_y, new_x]) + 
-                                  (1 - p) / 2 * (reward_grid[h-1-y, x] + discount_factor * value_function[y, x]))
-                else:
-                    values.append(reward_grid[h-1-y, x] + discount_factor * value_function[y, x])
-            best_action = np.argmax(values)
-            policy[y, x] = action_names[best_action]
 
     return policy, value_function
 
 if __name__ == "__main__":
-    w = 4
-    h = 3
-    L = [(1, 1, 0), (3, 2, 1), (3, 1, -1)]
-    p = 0.8
-    r = -0.04
-    policy, value_function = value_iteration(w, h, L, p, r)
-    print("Policy:\n", policy)
-    print("Value Function:\n", value_function)
+    iterator = GridWorldIterator('GridWorld.py')
+    for w ,h ,L,p,r in iterator:
+        policy, value_function = value_iteration(w, h, L, p, r)
+        print("Policy:\n", policy)
+        print("Value Function:\n", value_function)
+        
